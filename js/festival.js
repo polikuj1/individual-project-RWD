00aa0085ddbff9ee3075fc9bcc6431e6b11320aa
張嘉哲 <polikuj1@yahoo.com.tw>
2023-06-04 22:31:12 +0800

    音樂祭介紹修改中

--- js/festival.js
+++ js/festival.js
@@ -24,3 +24,28 @@ tab.addEventListener('click', (e) => {
   })
 });
 
+const tab_select = document.querySelector('.select_title');
+const now_area = document.querySelector('.select_title > span');
+const tab_option = document.querySelector('.option');
+const arrow = document.querySelector('.select_title .arrow');
+tab_select.onclick = function() {
+  if(tab_option.style.display === 'block') {
+    tab_option.style.display = 'none';
+    arrow.style.transform = `rotate(0deg)`;
+  } else {
+    tab_option.style.display = 'block';
+    arrow.style.transform = `rotate(90deg)`;
+  }
+}
+
+tab_option.addEventListener('click', (e) => {
+  let txt = e.target.textContent;
+  // now_area.textContent = txt;
+  festival.forEach((item) => {
+    if(item.dataset.id === txt) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  })
+})
